64e6f11feb9b1cbb2559a4a06ae7e4a6682a760e
Rachel Robinson <rachel73@mit.edu>
2023-09-21 04:27:40 -0400

    fixed login bug

--- src/concepts/websession.ts
+++ src/concepts/websession.ts
@@ -28,6 +28,7 @@ export default class WebSessionConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    this.isInActive(session);
     session.user = username;
   }
 
@@ -46,6 +47,13 @@ export default class WebSessionConcept {
   isActive(session: WebSessionDoc) {
     if (session.user === undefined) {
       throw new UnauthenticatedError("Not logged in!");
-    }
+    } 
   }
+
+  isInActive(session: WebSessionDoc) {
+    if (session.user !== undefined) {
+      throw new UnauthenticatedError("Already logged in!");
+    } 
+  }
+
 }
